a0af13511a5df5a1518f5baf6f473c1accc7c780
Kimon Pammer <kimonatwork@gmail.com>
2025-06-22 03:29:08 +0200

    feat: Implement two-pane core editor with file I/O

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,34 @@
+use std::fs;
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn read_file(path: String) -> String {
+    fs::read_to_string(path).unwrap()
+}
+
+#[tauri::command]
+fn write_file(path: String, contents: String) {
+    fs::write(path, contents).unwrap();
+}
+
+#[tauri::command]
+fn list_dir(path: String) -> Vec<String> {
+    let mut entries = Vec::new();
+    let read_dir = fs::read_dir(path).unwrap();
+
+    for entry in read_dir {
+        let dir_entry = entry.unwrap();
+        entries.push(dir_entry.path().display().to_string());
+    }
+
+    entries
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![read_file, write_file, list_dir])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
